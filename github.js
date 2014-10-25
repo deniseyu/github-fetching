734e9fc7f21256357b444523b198bbcbd227f7e9
Denise <yu.denise.d@gmail.com>
2014-10-25 23:35:23 +0100

    Set up JS and HTML files

---
+++ github.js
@@ -0,0 +1,10 @@
+  $('#fetch_github_profile').on('click', function(){
+      var profile_name = $('#profile_name').val();
+      var source = $('#profileTemplate').html();
+      var template = Handlebars.compile(source);
+      $.get('https://api.github.com/users/' + profile_name + '?client_id=?9a318764c4d9fd84b19d&client_secret=e46387ab354464abc3b973331ab2ca50f1ecc862', function(githubProfile) {
+        $('#profiles').append(template(githubProfile));
+
+    });
+
+  });
